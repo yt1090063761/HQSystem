804d83ac234bb9aaeb6820f138c7dca47415ae47
yt1090063761 <1090063761@qq.com>
2021-02-08 18:32:51 +0800

    默认的

---
+++ src/main/java/com/example/HQsystem/HQsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.HQsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HQsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HQsystemApplication.class, args);
+	}
+
+}
